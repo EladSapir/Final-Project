52c9f7126d06c33a9dd895283865a748d02bd245
Elad Sapir <elad.1999.1999@gmail.com>
2024-01-29 19:52:38 +0200

    new run of image classification

--- NewHenItaiSVC.py
+++ NewHenItaiSVC.py
@@ -1,16 +1,18 @@
 import numpy as np
 import os
 import cv2
-from sklearn import svm
-from sklearn.metrics import accuracy_score
 from tqdm import tqdm
+from sklearn import svm
+from sklearn.metrics import classification_report, accuracy_score
 from sklearn.utils import shuffle
+import pandas as pd
+import matplotlib.pyplot as plt
 
 class_names = ['mountain', 'street', 'glacier']
 class_names_label = {class_name: i for i, class_name in enumerate(class_names)}
 
 IMAGE_SIZE = (150, 150)
-BATCH_SIZE = 100  # Adjust this based on your server's memory capacity
+BATCH_SIZE = 100  # You can adjust this based on your server's memory capacity
 
 def process_images(dataset, folder, start, end):
     images = []
@@ -52,16 +54,13 @@ def load_data():
 (train_images, train_labels), (test_images, test_labels) = load_data()
 train_images, train_labels = shuffle(train_images, train_labels, random_state=25)
 
-
-train_images = train_images / 255.0
-test_images = test_images / 255.0
-
 train_images = train_images.reshape(-1, 150*150*3)
 test_images = test_images.reshape(-1, 150*150*3)
 
-# Parameters to iterate over
-clf = svm.SVC(C=4, kernel='poly', degree=3 ,cache_size=8000)
+# SVM Classifier
+clf = svm.SVC(C=10, kernel='poly', degree=3, cache_size=8000)
 clf.fit(train_images, train_labels)
 predictions = clf.predict(test_images)
 accuracy = accuracy_score(test_labels, predictions)
 
+print(f"Accuracy: {accuracy}")
